56184b990c83d4f82def58c284fbb470d24a40e6
IvelinL <157578504+IVELINCHO2024@users.noreply.github.com>
2026-03-26 12:12:14 +0200

    protection with limiter

--- backend/routing/authRoutes.ts
+++ backend/routing/authRoutes.ts
@@ -1,10 +1,10 @@
 import Router from "express";
 import {register, getProfile, changeRole} from "../controllers/authController";
-import {verifyToken} from "../middleware/middleware";
+import {verifyToken, authLimiter} from "../middleware/middleware";
 
 const router = Router();
 
-router.post("/sign_up", register);
+router.post("/sign_up", authLimiter, register);
 router.get("/profile", verifyToken, getProfile);
 router.post("/change_role", verifyToken, changeRole);
 
