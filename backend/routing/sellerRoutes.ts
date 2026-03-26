ffa53324c44ad4425a28be73bc6b00eee424eb34
IvelinL <157578504+IVELINCHO2024@users.noreply.github.com>
2026-03-26 13:11:28 +0200

    rate limiter for seller routes

--- backend/routing/sellerRoutes.ts
+++ backend/routing/sellerRoutes.ts
@@ -1,12 +1,12 @@
 import Router from "express";
 import {register, getProfile} from "../controllers/authController";
-import {verifyToken} from "../middleware/middleware";
+import {sellerLimiter, verifyToken} from "../middleware/middleware";
 import {productListing, listingConfirmation, getProducts} from "../controllers/sellerControllers";
 
 const router = Router();
 
-router.post("/product",verifyToken, productListing);
-router.post("/confirmation",verifyToken, listingConfirmation);
-router.get("/getProducts", verifyToken, getProducts)
+router.post("/product",verifyToken,sellerLimiter, productListing);
+router.post("/confirmation",verifyToken,sellerLimiter, listingConfirmation);
+router.get("/getProducts", verifyToken,sellerLimiter, getProducts)
 
 export default router;
